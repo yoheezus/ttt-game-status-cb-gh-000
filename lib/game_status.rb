8beef4b3b6b6a95e2c201e17aea08eace7d8e90f
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-03 01:53:16 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -42,7 +42,7 @@ def draw?(board)
     diagonal1.each{|i| diag_win1 << board[i]}
     diagonal2.each{|i| diag_win2 << board[i]}
     print  diag_win1, diag_win2
-    unless diag_win1.all? == "X" || diag_win1.all? == "O" || diag_win2.all? == "O" || diag_win2.all? == "X"
+    if diag_win1.all? == "X" || diag_win1.all? == "O" || diag_win2.all? == "O" || diag_win2.all? == "X"
         false
     end
     if full?(board) && won?(board) == false
