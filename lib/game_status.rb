b49e964adc94cb90480f4f0e3f439e25d9cf1a43
Yohann Lovell <yoyunyo@hotmail.co.uk>
2019-01-03 02:14:01 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -36,7 +36,7 @@ def full?(board)
 end
 
 def draw?(board)
-    if full?(board) && won?(board) == false
+    if full?(board) && !won?(board)
         true
     elsif diagonal_win?(board)
         return false
